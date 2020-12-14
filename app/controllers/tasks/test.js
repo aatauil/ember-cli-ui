3677d9b22f56e281150d2b09de4cbb7953e422d0
achraf <achrafatauil@hotmail.com>
2020-12-14 22:45:46 +0100

    add tasks/test controller

---
+++ app/controllers/tasks/test.js
@@ -0,0 +1,33 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class TasksTestController extends Controller {
+  @tracked command;
+  @tracked type = "ALL";
+  @tracked filter;
+
+  commands = {
+    ALL: 'npm test',
+    Unit: 'ember t -f="Unit"',
+    Integration: 'ember t -f="Integration"',
+    Acceptance: 'ember t -f="Acceptance"',
+    'Custom Filter': '',
+  };
+
+  @action
+  runTests() {
+    if(this.type === 'Custom') {
+    this.command = `ember t -f="${this.filter}"\r\n`;
+    } else  {
+      this.command = `${this.commands[this.type]}\r\n`;
+    }
+  }
+
+  @action
+  updateType(event) {
+    this.type = event.target.value;
+  }
+
+}
+
