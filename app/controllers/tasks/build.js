af709f2bb92c019f19f9ed1b39b5e8f4c7077062
achraf <achrafatauil@hotmail.com>
2020-12-14 23:09:26 +0100

    create tasks/build controller

---
+++ app/controllers/tasks/build.js
@@ -0,0 +1,41 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { htmlSafe } from '@ember/template';
+
+export default class TasksBuildController extends Controller {
+  @tracked disabled = false;
+  @tracked showSpinner = false;
+  @tracked assets = [];
+  @tracked showLog = false;
+  @tracked logs = [];
+  @tracked buildReady = false;
+
+  @action
+  runBuild() {
+    this.showSpinner = true;
+    this.disabled = true;
+    fetch('/build')
+      .then(res => res.json())
+      .then(response => {
+        //console.log(response);
+        this.logs = response.logs.map(l => htmlSafe(l));
+        this.assets = response.assets;
+
+        this.showSpinner = false;
+        this.disabled = false;
+        this.buildReady = true;
+      });
+
+  }
+
+  @action
+  toggleLog() {
+    this.showLog = !this.showLog;
+  }
+
+  get logBtnLabel() {
+    return this.showLog ? 'Hide Log' : 'Show Log';
+  }
+
+}
